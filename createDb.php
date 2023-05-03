163d42d31e8fda065de57e3d8e716721ff48c063
DUCAU Lucas <lucas.ducau64@gmail.com>
2023-05-03 09:37:19 +0200

    2.7

--- createDb.php
+++ createDb.php
@@ -6,3 +6,10 @@ echo "Create DB";
 $pdo = new PDO("mysql:host=localhost;port=3306;charset=utf8", "admin", "admin");
 
 dump($pdo);
+
+
+$sql = "CREATE DATABASE IF NOT EXISTS composer_users";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute();
